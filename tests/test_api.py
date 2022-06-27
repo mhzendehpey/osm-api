82058930e89905e4188a853e1653d22e3d2bd497
M.Hossein <m.h.zendehpey@gmail.com>
2022-06-27 12:25:49 +0430

    Update tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,23 +2,43 @@ import pytest
 from osm_api.api import get_url, get_object
 
 
+class OsmObject():
+
+    _type = str
+    _id = str
+
+    def __init__(self, osm_type: str, osm_id: str):
+        self._type = osm_type
+        self._id = osm_id
+
+
+relation = OsmObject('relation', '537701')
+way = OsmObject('way', '440576851')
+node = OsmObject('node', '25960293')
+
+
 class TestGetUrl():
 
     def test_get_url_relation(self):
         assert get_url(
-            'relation', '537701') == 'https://www.openstreetmap.org/api/0.6/relation/537701/full/'
+            relation._type, relation._id) == 'https://www.openstreetmap.org/api/0.6/relation/537701/full/'
 
     def test_get_url_way(self):
         assert get_url(
-            'way', '440576851') == 'https://www.openstreetmap.org/api/0.6/way/440576851/full/'
+            way._type, way._id) == 'https://www.openstreetmap.org/api/0.6/way/440576851/full/'
 
     def test_get_url_node(self):
         assert get_url(
-            'node', '25960293') == 'https://www.openstreetmap.org/api/0.6/node/25960293/'
+            node._type, node._id) == 'https://www.openstreetmap.org/api/0.6/node/25960293/'
 
 
 class TestGetObject:
 
-    def test_get_object(self):
+    def test_get_object_invalid_id(self):
         with pytest.raises(Exception):
-            get_object('relation', '584565')
+            get_object(relation._type, way._id)
+
+    def test_get_object_valid_id(self):
+        assert get_object(relation._type, relation._id).status_code == 200
+        assert get_object(way._type, way._id).status_code == 200
+        assert get_object(node._type, node._id).status_code == 200
